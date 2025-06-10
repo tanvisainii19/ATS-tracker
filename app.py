a0cfddd960b9007b597c09d16978d31598011b15
tanvisainii19 <saini1906tanvi@gmail.com>
2025-06-10 23:27:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,26 @@
-from dotenv import load_dotenv
-
-load_dotenv() 
-import stramlit as st
+import streamlit as st
+import google.generativeai as genai
 import os
 import PyPDF2 as pdf
-import google.generativeai as genai
+from dotenv import load_dotenv
+import json
+
+load_dotenv() ## load all our environment variables
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-def get_gemini_response(input,):
+def get_gemini_repsonse(input):
     model=genai.GenerativeModel('gemini-pro')
     response=model.generate_content(input)
     return response.text
 
 def input_pdf_text(uploaded_file):
-   reader=pdf.PdfReader(uploaded_file)
-   text=""
-   for page in reader(len(reader.pages)):
-      page=reader.pages[page]
-      text+=str(page.extract_text())
-      return text
+    reader=pdf.PdfReader(uploaded_file)
+    text=""
+    for page in range(len(reader.pages)):
+        page=reader.pages[page]
+        text+=str(page.extract_text())
+    return text
 
 #Prompt Template
 
